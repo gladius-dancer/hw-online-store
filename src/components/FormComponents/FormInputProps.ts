fbc02870c493d2709f16a8bac3b9c07a874fe733
Rustem <sernazarovrustem@gmail.com>
2023-03-15 02:12:57 +0500

    Second

---
+++ src/components/FormComponents/FormInputProps.ts
@@ -0,0 +1,6 @@
+export interface FormInputProps {
+  name: string;
+  control: any;
+  setValue?: any;
+  status: boolean
+}
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import images from "../../assets/images";
 import { changeNavAction } from "../../store/changeNavReduser";
+import { InputText } from "../../components/FormComponents/InputText";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function Login() {
+
+  const schema = yup.object().shape({
+    login: yup.string().required(),
+    password: yup.string().required().min(4),
+  });
+
+
+  const methods = useForm({resolver: yupResolver(schema)});
+  const {handleSubmit, control, setValue, formState: {errors}} = methods;
+  const onSubmit = (data: any) => console.log(data);
+
+
   return (
     <div>
       <header className="header_area">
         <div className="top_header_area">
           <div className="container h-100">
-            <div className="row h-100 align-items-center justify-content-end">
-
+            <div className="row h-100 align-items-center justify-content-center">
               <div className="col-12 col-lg-7">
-                <div className="top_single_area d-flex align-items-center justify-content-between">
+                <div className="top_single_area d-flex align-items-center justify-content-center">
                   {/* Logo Area */}
-                  <div className="top_logo">
+                  <div className="logo">
                     <a href="#"><img src={images.logo} alt=""/></a>
                   </div>
-
-
-
                 </div>
               </div>
 
@@ -27,20 +39,17 @@ function Login() {
         </div>
       </header>
       <section className="login-form d-flex justify-content-center align-content-center">
-          <div className="checkout_details_area mt-30 mb-50 mx-auto clearfix col-md-4">
+          <div className="checkout_details_area mb-50 mx-auto clearfix col-md-4">
 
-            <div className="cart-page-heading">
-              <h5>Login</h5>
-            </div>
-            <form action="#" method="post">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="column">
                 <div className="col-md-12 mb-3">
                   <label htmlFor="first_name">Login <span>*</span></label>
-                  <input type="text" className="form-control" id="first_name" required/>
+                  <InputText key="name" name="login" control={control} status={true}/>
                 </div>
                 <div className="col-md-12 mb-3">
                   <label htmlFor="last_name">Password <span>*</span></label>
-                  <input type="text" className="form-control" id="last_name" required/>
+                  <InputText key="password" name="password" control={control} status={true}/>
                 </div>
                 <button className="karl-checkout-btn">Login</button>
 
@@ -48,9 +57,8 @@ function Login() {
             </form>
         </div>
       </section>
-      <footer className="footer_area">
+      <footer className="mt-20 mb-50">
         <div className="container">
-          <div className="footer_bottom_area">
             <div className="row">
               <div className="col-12">
                 <div className="footer_social_area text-center">
@@ -61,7 +69,6 @@ function Login() {
                 </div>
               </div>
             </div>
-          </div>
         </div>
       </footer>
     </div>

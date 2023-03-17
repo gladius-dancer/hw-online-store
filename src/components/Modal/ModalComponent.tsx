a81a1025d7eec12e8958e22e361aca0ea5e8086b
Rustem <sernazarovrustem@gmail.com>
2023-03-17 14:03:45 +0500

    Eighth

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -4,17 +4,15 @@ import Modal from "react-modal";
 type ModalProps = {
   isOpen: boolean,
   children: any,
-  style: any,
   className: string,
   overlayClassName: string
 }
 
-export default function ModalComponent({ isOpen, children, style, className, overlayClassName }: ModalProps) {
+export default function ModalComponent({ isOpen, children, className, overlayClassName }: ModalProps) {
 
   return (
     <Modal
       isOpen={isOpen}
-      style={style}
       className={className}
       overlayClassName={overlayClassName}
     >
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchProducts } from "../../store/actions";
 import { useAppDispatch, useAppSelector } from "../../store/store";
 import { addProductAction, getProductsAction, updateProductsAction } from "../../store/cartReduser";
 import { setPriceAction } from "../../store/priceReduser";
+import {ProductType} from "../../types/ProductType";
 import ModalComponent from "../Modal/ModalComponent";
+import "./Products.scss";
 
 
 function Products() {
   const dispatch = useAppDispatch();
   const products = useAppSelector(state => state.products.products);
   const cart = useAppSelector(state => state.cart);
+  const [modal, setModal] = useState(false);
+  const [currentProduct, setSetCurrentProduct] = useState({});
 
   const addToCart = (id: number) => {
     const product = products.filter((item: any) => item.id === id)[0];
@@ -30,6 +34,12 @@ function Products() {
 
     dispatch(setPriceAction({ totalPrice: totalPrice, shipping: 0 }));
   };
+
+  const showDetails = (id: number)=>{
+    setModal(true)
+    const current: ProductType = products.filter((product: any)=>product.id === id)[0];
+    setSetCurrentProduct(current);
+  }
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -72,7 +82,7 @@ function Products() {
               {/* ProductType Image */}
               <div className="product-img">
                 <img src={item.image} alt="" />
-                <div className="product-quicview">
+                <div className="product-quicview" onClick={()=>showDetails(item.id)}>
                   <a href="#" data-toggle="modal" data-target="#quickview"><i className="ti-plus"></i></a>
                 </div>
               </div>
@@ -90,17 +100,15 @@ function Products() {
       </div>
 
       <ModalComponent
-        isOpen={true}
-        className="modal-content"
+        isOpen={modal}
+        className="modal"
         overlayClassName="modal-overlay"
-        style="hello"
-      >
-
-        <div className="modal fade" id="quickview" role="dialog" aria-labelledby="quickview"
+        >
+        <div className="" id="quickview" role="dialog" aria-labelledby="quickview"
              aria-hidden="true">
           <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
             <div className="modal-content">
-              <button type="button" className="close btn" data-dismiss="modal" aria-label="Close">
+              <button onClick={()=>setModal(false)} type="button" className="close btn" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
 
@@ -108,14 +116,14 @@ function Products() {
                 <div className="quickview_body">
                   <div className="container">
                     <div className="row">
-                      <div className="col-12 col-lg-5">
+                      <div className="col-12 col-lg-5 d-flex align-items-center">
                         <div className="quickview_pro_img">
-                          <img src="img/product-img/product-1.jpg" alt="" />
+                          <img src={currentProduct.image} alt="" />
                         </div>
                       </div>
                       <div className="col-12 col-lg-7">
                         <div className="quickview_pro_des">
-                          <h4 className="title">Boutique Silk Dress</h4>
+                          <h4 className="title">{currentProduct.title}</h4>
                           <div className="top_seller_product_rating mb-15">
                             <i className="fa fa-star" aria-hidden="true"></i>
                             <i className="fa fa-star" aria-hidden="true"></i>
@@ -123,25 +131,13 @@ function Products() {
                             <i className="fa fa-star" aria-hidden="true"></i>
                             <i className="fa fa-star" aria-hidden="true"></i>
                           </div>
-                          <h5 className="price">$120.99 <span>$130</span></h5>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia expedita quibusdam
-                            aspernatur, sapiente consectetur accusantium perspiciatis praesentium eligendi, in
-                            fugiat?</p>
+                          <h5 className="price">$ {currentProduct.price} <span>$ {parseFloat(currentProduct.price * 130 / 100).toFixed(2)}</span></h5>
+                          <p>{currentProduct.description}</p>
                           <a href="#">View Full Product Details</a>
                         </div>
                         {/* Add to Cart Form */}
-                        <form className="cart" method="post">
-                          <div className="quantity">
-                            <span className="qty-minus">
-                              <i className="fa fa-minus" aria-hidden="true"></i></span>
-
-                            <input type="number" className="qty-text" id="qty" step="1" min="1" max="12" name="quantity"
-                                   value="1" />
-
-                            <span className="qty-plus">
-                                <i className="fa fa-plus" aria-hidden="true"></i></span>
-                          </div>
-                          <button type="submit" name="addtocart" value="5" className="cart-submit">Add to cart</button>
+                        <div className="cart">
+                          <button onClick={() => addToCart(currentProduct.id)} name="addtocart" className="cart-submit">Add to cart</button>
                           {/* Wishlist */}
                           <div className="modal_pro_wishlist">
                             <a href="wishlist.html" target="_blank"><i className="ti-heart"></i></a>
@@ -150,7 +146,7 @@ function Products() {
                           <div className="modal_pro_compare">
                             <a href="compare.html" target="_blank"><i className="ti-stats-up"></i></a>
                           </div>
-                        </form>
+                        </div>
 
                         <div className="share_wf mt-30">
                           <p>Share With Friend</p>
